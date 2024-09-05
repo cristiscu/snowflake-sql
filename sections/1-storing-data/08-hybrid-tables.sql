3ae682ddc1ce29b1319ef585c1e2c64bed901f14
cristiscu <cristiscu@gmail.com>
2024-09-04 18:09:52 -0700

    updates

---
+++ sections/1-storing-data/08-hybrid-tables.sql
@@ -0,0 +1,22 @@
+use test.employees;
+
+create or replace hybrid table projh (
+	proj_id     integer     PRIMARY KEY,
+	name        string      UNIQUE,
+	start_date  date,
+    end_date    date);
+
+create or replace index projh_dates (start_date, end_date) on projh;
+
+insert into projh values
+    (1,    'Cleanup Data',          '1980-12-05',   '1981-01-09'    ),
+    (2,    'ETL Pipeline',          '1981-01-09',   '1981-04-02'    ),
+    (3,    'Data Preprocessing',    '1981-04-02',   '1981-06-08'    ),
+    (4,    'Create Dashboard',      '1981-06-09',   '1981-07-22'    ),
+    (5,    'ML Kickoff',            '1981-08-28',   '1981-09-11'    ),
+    (6,    'Model Training',        '1981-09-28',   '1982-12-10'    ),
+    (7,    'Model Deployment',      '1982-12-11',   null            );
+
+-- these will fail!
+insert into projh values (1, 'Cleanup Data 2', '1980-12-05', '1981-01-09');
+insert into projh values (10, 'Cleanup Data', '1980-12-05', '1981-01-09');
