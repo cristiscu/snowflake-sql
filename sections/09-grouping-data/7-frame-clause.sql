42f5630cd1a792b1eadc645ddcf9de7c467ec4ea
cristiscu <cristiscu@gmail.com>
2026-02-24 15:34:35 +0200

    renumbered chapters

--- sections/09-grouping-data/7-frame-clause.sql
+++
@@ -1,8 +0,0 @@
-use schema test.employees;
-
--- window frame (--> moving average)
-select name, hire_date, salary,
-    round(avg(salary) over (order by hire_date
-        rows between 2 preceding and current row), 2) as avg
-from emp
-order by hire_date;
