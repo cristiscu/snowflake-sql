8909a70795ea3595fa5aeac51a5303b94d9b7a64
cristiscu <cristiscu@gmail.com>
2026-02-25 06:15:25 +0200

    immutability constraints

---
+++ sections/12-etl-elt-pipelines/6-immutability-constraints.sql
@@ -0,0 +1,22 @@
+-- Dynamic tables with immutability constraints
+-- https://www.snowflake.com/en/engineering-blog/dynamic-tables-immutability/
+-- https://docs.snowflake.com/en/user-guide/dynamic-tables-immutability-constraints
+use test.employees;
+
+CREATE TABLE tx (id INT, ts TIMESTAMP, amount INT);
+
+CREATE DYNAMIC TABLE tx_stats
+    TARGET_LAG = '1 minute'
+    WAREHOUSE = compute_wh
+    IMMUTABLE WHERE (hour < CURRENT_TIMESTAMP() - INTERVAL '1 day')
+AS
+    SELECT DATE_TRUNC(HOUR, ts) as hour, SUM(amount) as total_amount
+    FROM tx
+    GROUP BY hour;
+
+CREATE TASK tx_delete_old
+    SCHEDULE = '12 hours'
+    WAREHOUSE = compute_wh
+AS
+    DELETE FROM tx
+    WHERE ts < CURRENT_TIMESTAMP() - INTERVAL '1 week';
